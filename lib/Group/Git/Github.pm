2685ba181016790037a34cce41e6123f333a159b
Ivan Wills <ivan.wills@gmail.com>
2013-05-06 21:28:42 +1000

    Added prompting for username/password if missing, started documenting

--- lib/Group/Git/Github.pm
+++ lib/Group/Git/Github.pm
@@ -46,10 +46,11 @@ sub _repos {
 
 sub _github {
     my ($self) = @_;
+    my $conf = $self->conf;
 
     return Net::GitHub->new(
-        login => $self->conf->{username},
-        pass  => $self->conf->{password},
+        login => $conf->{username} ? $conf->{username} : prompt( -prompt => 'github.com username : ' );,
+        pass  => $conf->{password} ? $conf->{password} : prompt( -prompt => 'github.com password : ', -echo => '*' );,
     );
 }
 
@@ -59,7 +60,7 @@ __END__
 
 =head1 NAME
 
-Group::Git::Github - <One-line description of module's purpose>
+Group::Git::Github - Adds reading all repositories you have access to on github
 
 =head1 VERSION
 
@@ -70,73 +71,30 @@ This documentation refers to Group::Git::Github version 0.1.
 
    use Group::Git::Github;
 
-   # Brief but working code example(s) here showing the most common usage(s)
-   # This section will be as far as many users bother reading, so make it as
-   # educational and exemplary as possible.
-
+   # pull (or clone missing) all repositories that joeblogs has created/forked
+   Group::Git::Github->new(
+       conf => {
+           username => 'joeblogs@gmail.com',
+           password => 'myverysecurepassword',
+       },
+   )->pull;
 
 =head1 DESCRIPTION
 
-A full description of the module and its features.
-
-May include numerous subsections (i.e., =head2, =head3, etc.).
-
+Reads all repositories for the configured user (if none set user will be
+prompted to enter one as well as a password)
 
 =head1 SUBROUTINES/METHODS
 
-A separate section listing the public components of the module's interface.
-
-These normally consist of either subroutines that may be exported, or methods
-that may be called on objects belonging to the classes that the module
-provides.
-
-Name the section accordingly.
-
-In an object-oriented module, this section should begin with a sentence (of the
-form "An object of this class represents ...") to give the reader a high-level
-context to help them understand the methods that are subsequently described.
-
-
-
-
 =head1 DIAGNOSTICS
-
-A list of every error and warning message that the module can generate (even
-the ones that will "never happen"), with a full explanation of each problem,
-one or more likely causes, and any suggested remedies.
 
 =head1 CONFIGURATION AND ENVIRONMENT
 
-A full explanation of any configuration system(s) used by the module, including
-the names and locations of any configuration files, and the meaning of any
-environment variables or properties that can be set. These descriptions must
-also include details of any configuration language used.
-
 =head1 DEPENDENCIES
-
-A list of all of the other modules that this module relies upon, including any
-restrictions on versions, and an indication of whether these required modules
-are part of the standard Perl distribution, part of the module's distribution,
-or must be installed separately.
 
 =head1 INCOMPATIBILITIES
 
-A list of any modules that this module cannot be used in conjunction with.
-This may be due to name conflicts in the interface, or competition for system
-or program resources, or due to internal limitations of Perl (for example, many
-modules that use source code filters are mutually incompatible).
-
 =head1 BUGS AND LIMITATIONS
-
-A list of known problems with the module, together with some indication of
-whether they are likely to be fixed in an upcoming release.
-
-Also, a list of restrictions on the features the module does provide: data types
-that cannot be handled, performance issues and the circumstances in which they
-may arise, practical limitations on the size of data sets, special cases that
-are not (yet) handled, etc.
-
-The initial template usually just has:
 
 There are no known bugs in this module.
 
