87aca290cfb23afd661579124e5a7994ba96ca2c
Ivan Wills <ivan.wills@gmail.com>
2013-05-06 21:27:58 +1000

    Fixed getting repositories, Started documentation

--- lib/Group/Git/Bitbucket.pm
+++ lib/Group/Git/Bitbucket.pm
@@ -12,11 +12,18 @@ use Carp;
 use Data::Dumper qw/Dumper/;
 use English qw/ -no_match_vars /;
 use IO::Prompt qw/prompt/;
+use JSON qw/decode_json/;
 use WWW::Mechanize;
 
 our $VERSION     = version->new('0.0.1');
 
 extends 'Group::Git';
+
+sub _httpenc($) {
+    my ($str) = @_;
+    $str =~ s/(\W)/sprintf "%%%x", ord $1/egxms;
+    return $str;
+}
 
 sub _repos {
     my ($self) = @_;
@@ -28,22 +35,22 @@ sub _repos {
     my @argv = @ARGV;
     @ARGV = ();
     my $mech = WWW::Mechanize->new;
-    my $user = $conf->{user} ? $conf->{user} : httpenc prompt( -prompt => 'bitbucket username : ' );
-    my $pass = $conf->{pass} ? $conf->{pass} : httpenc prompt( -prompt => 'bitbucket password : ', -echo => '*' );
+    my $user = _httpenc( $conf->{username} ? $conf->{username} : prompt( -prompt => 'bitbucket username : ' ) );
+    my $pass = _httpenc( $conf->{password} ? $conf->{password} : prompt( -prompt => 'bitbucket password : ', -echo => '*' ) );
     my $url  = "https://$user:$pass\@api.bitbucket.org/1.0/user/repositories";
     @ARGV = @argv;
 
     $mech->get($url);
     my $repos = decode_json $mech->content;
     for my $repo ( @$repos ) {
-        $repos{$repo->{name}} = {
+        $repos{$repo->{name}} = Group::Git::Repo->new(
             url => "https://bitbucket.org/$repo->{owner}/$repo->{name}",
             git => "git\@bitbucket.org:$repo->{owner}/$repo->{name}.git",
-        };
+        );
     }
-    return %repos;
-}
 
+    return \%repos;
+}
 
 1;
 
@@ -51,84 +58,40 @@ __END__
 
 =head1 NAME
 
-Group::Git::Bitbucket - <One-line description of module's purpose>
+Group::Git::Bitbucket - Adds reading all repositories you have access to on bitbucket
 
 =head1 VERSION
 
 This documentation refers to Group::Git::Bitbucket version 0.1.
 
-
 =head1 SYNOPSIS
 
    use Group::Git::Bitbucket;
 
-   # Brief but working code example(s) here showing the most common usage(s)
-   # This section will be as far as many users bother reading, so make it as
-   # educational and exemplary as possible.
-
+   # pull (or clone missing) all repositories that joeblogs has created/forked
+   Group::Git::Bitbucket->new(
+       conf => {
+           username => 'joeblogs@gmail.com',
+           password => 'myverysecurepassword',
+       },
+   )->pull;
 
 =head1 DESCRIPTION
 
-A full description of the module and its features.
-
-May include numerous subsections (i.e., =head2, =head3, etc.).
-
+Reads all repositories for the configured user (if none set user will be
+prompted to enter one as well as a password)
 
 =head1 SUBROUTINES/METHODS
 
-A separate section listing the public components of the module's interface.
-
-These normally consist of either subroutines that may be exported, or methods
-that may be called on objects belonging to the classes that the module
-provides.
-
-Name the section accordingly.
-
-In an object-oriented module, this section should begin with a sentence (of the
-form "An object of this class represents ...") to give the reader a high-level
-context to help them understand the methods that are subsequently described.
-
-
-
-
 =head1 DIAGNOSTICS
-
-A list of every error and warning message that the module can generate (even
-the ones that will "never happen"), with a full explanation of each problem,
-one or more likely causes, and any suggested remedies.
 
 =head1 CONFIGURATION AND ENVIRONMENT
 
-A full explanation of any configuration system(s) used by the module, including
-the names and locations of any configuration files, and the meaning of any
-environment variables or properties that can be set. These descriptions must
-also include details of any configuration language used.
-
 =head1 DEPENDENCIES
-
-A list of all of the other modules that this module relies upon, including any
-restrictions on versions, and an indication of whether these required modules
-are part of the standard Perl distribution, part of the module's distribution,
-or must be installed separately.
 
 =head1 INCOMPATIBILITIES
 
-A list of any modules that this module cannot be used in conjunction with.
-This may be due to name conflicts in the interface, or competition for system
-or program resources, or due to internal limitations of Perl (for example, many
-modules that use source code filters are mutually incompatible).
-
 =head1 BUGS AND LIMITATIONS
-
-A list of known problems with the module, together with some indication of
-whether they are likely to be fixed in an upcoming release.
-
-Also, a list of restrictions on the features the module does provide: data types
-that cannot be handled, performance issues and the circumstances in which they
-may arise, practical limitations on the size of data sets, special cases that
-are not (yet) handled, etc.
-
-The initial template usually just has:
 
 There are no known bugs in this module.
 
