15b892d7ed6c27e6c029c9f9bf1facb6b2e1f8e4
Ivan Wills <ivan.wills@gmail.com>
2013-05-24 19:28:46 +1000

    Updated version for release

--- lib/Group/Git/Bitbucket.pm
+++ lib/Group/Git/Bitbucket.pm
@@ -15,7 +15,7 @@ use IO::Prompt qw/prompt/;
 use JSON qw/decode_json/;
 use WWW::Mechanize;
 
-our $VERSION     = version->new('0.0.4');
+our $VERSION     = version->new('0.0.5');
 
 extends 'Group::Git';
 
@@ -63,7 +63,7 @@ Group::Git::Bitbucket - Adds reading all repositories you have access to on bitb
 
 =head1 VERSION
 
-This documentation refers to Group::Git::Bitbucket version 0.0.4.
+This documentation refers to Group::Git::Bitbucket version 0.0.5.
 
 =head1 SYNOPSIS
 
--- lib/Group/Git/Gitosis.pm
+++ lib/Group/Git/Gitosis.pm
@@ -14,7 +14,7 @@ use English qw/ -no_match_vars /;
 use Path::Class;
 use File::chdir;
 
-our $VERSION     = version->new('0.0.4');
+our $VERSION     = version->new('0.0.5');
 
 extends 'Group::Git';
 
@@ -76,7 +76,7 @@ Group::Git::Gitosis - <One-line description of module's purpose>
 
 =head1 VERSION
 
-This documentation refers to Group::Git::Gitosis version 0.0.4.
+This documentation refers to Group::Git::Gitosis version 0.0.5.
 
 
 =head1 SYNOPSIS
