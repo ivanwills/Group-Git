a34819b7957b59f45d008ef85319eedf249be6ce
Ivan Wills <ivan.wills@gmail.com>
2013-06-13 07:21:19 +1000

    Updated version for next release

--- lib/Group/Git.pm
+++ lib/Group/Git.pm
@@ -15,7 +15,7 @@ use Path::Class;
 use File::chdir;
 use Group::Git::Repo;
 
-our $VERSION     = version->new('0.1.2');
+our $VERSION     = version->new('0.1.3');
 our $AUTOLOAD;
 
 has conf => (
@@ -133,7 +133,7 @@ Group::Git - Base module for group of git repository operations.
 
 =head1 VERSION
 
-This documentation refers to Group::Git version 0.1.2.
+This documentation refers to Group::Git version 0.1.3.
 
 =head1 SYNOPSIS
 
