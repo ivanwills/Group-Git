d87a568ffe071d37dc9de5199f905bb5337ca9d7
Ivan Wills <ivan.wills@gmail.com>
2015-07-06 15:19:02 +1000

    Updating for release of version 0.5.3

--- lib/Group/Git/Stash.pm
+++ lib/Group/Git/Stash.pm
@@ -17,7 +17,7 @@ use JSON qw/decode_json/;
 use WWW::Mechanize;
 use Path::Class;
 
-our $VERSION = version->new('0.5.2');
+our $VERSION = version->new('0.5.3');
 
 extends 'Group::Git';
 
@@ -82,7 +82,7 @@ Group::Git::Stash - Adds reading all repositories you have access to on your loc
 
 =head1 VERSION
 
-This documentation refers to Group::Git::Stash version 0.5.2.
+This documentation refers to Group::Git::Stash version 0.5.3.
 
 =head1 SYNOPSIS
 
