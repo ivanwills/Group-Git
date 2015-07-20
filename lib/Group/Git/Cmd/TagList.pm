a376c049dc1de38d99810a08b34271807a64f3b0
Ivan Wills <ivan.wills@gmail.com>
2015-07-20 10:30:00 +1000

    Updating for release of version 0.5.4

--- lib/Group/Git/Cmd/TagList.pm
+++ lib/Group/Git/Cmd/TagList.pm
@@ -15,7 +15,7 @@ use English qw/ -no_match_vars /;
 use File::chdir;
 use Getopt::Alt;
 
-our $VERSION = version->new('0.5.3');
+our $VERSION = version->new('0.5.4');
 
 requires 'repos';
 requires 'verbose';
@@ -66,7 +66,7 @@ Group::Git::Cmd::TagList - Runs git status on a git project
 
 =head1 VERSION
 
-This documentation refers to Group::Git::Cmd::TagList version 0.5.3.
+This documentation refers to Group::Git::Cmd::TagList version 0.5.4.
 
 =head1 SYNOPSIS
 
