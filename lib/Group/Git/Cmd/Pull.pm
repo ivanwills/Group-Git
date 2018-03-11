5ed08a87b43eb8af2d49b9aef1c4774e356e4ce9
Ivan Wills <Ivan.Wills2@optus.com.au>
2018-03-12 07:04:36 +1100

    Updating for release of version 0.6.7

--- lib/Group/Git/Cmd/Pull.pm
+++ lib/Group/Git/Cmd/Pull.pm
@@ -15,7 +15,7 @@ use English qw/ -no_match_vars /;
 use File::chdir;
 use Getopt::Alt;
 
-our $VERSION = version->new('0.6.6');
+our $VERSION = version->new('0.6.7');
 
 requires 'repos';
 requires 'verbose';
@@ -121,7 +121,7 @@ Group::Git::Cmd::Pull - Pull latest versions of all repositories or clone any th
 
 =head1 VERSION
 
-This documentation refers to Group::Git::Cmd::Pull version 0.6.6.
+This documentation refers to Group::Git::Cmd::Pull version 0.6.7.
 
 =head1 SYNOPSIS
 
