fd956ea236f3ba0f67ac080ba8b35ca1007af25f
Ivan Wills <Ivan.Wills2@optus.com.au>
2018-04-30 09:40:40 +1000

    Fixing getting the directory name for tags

--- lib/Group/Git/Stash.pm
+++ lib/Group/Git/Stash.pm
@@ -60,10 +60,13 @@ sub _repos {
             my $project = $repo->{project}{name};
             my $url     = $repo->{links}{self}[0]{href};
             my %clone   = map {($_->{name} => $_->{href})} @{ $repo->{links}{clone} };
-            my $dir     = $self->recurse ? path("$project/$repo->{name}") : path($repo->{name});
+            my $git     = $conf->{clone_type} && $conf->{clone_type} eq 'http' ? $clone{http} : $clone{ssh};
+            my ($dir)   = $self->recurse ? $git =~ m{([^/]+/[^/]+?)(?:[.]git)?$} : $git =~ m{/([^/]+?)(?:[.]git)?$};
+            my $name    = $self->recurse ? path("$project/$repo->{name}") : path($repo->{name});
+            $dir =~ s/^~//xms;
 
             $repos{$dir} = Group::Git::Repo->new(
-                name => $dir,
+                name => path($dir),
                 url  => $url,
                 git  => $conf->{clone_type} && $conf->{clone_type} eq 'http' ? $clone{http} : $clone{ssh},
             );
@@ -72,6 +75,9 @@ sub _repos {
 
             if ( $repo->{project}{owner} ) {
                 push @{ $conf->{tags}{personal} }, "$dir";
+            }
+            else {
+                push @{ $conf->{tags}{project} }, "$dir";
             }
         }
         $more  = !$response->{isLastPage};
